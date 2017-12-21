b8a593be7f4e1081a2dd478412b00e9093dc2cde
vishnunc <31437273+vishnunc@users.noreply.github.com>
2017-12-20 22:23:12 -0800

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,3 +1,7 @@
 def prep(){
+  def me =sh (script:'git diff HEAD^ HEAD --name-only',returnStdout:true).trim().split('/')
+  def pckg= me.length>1?me[1]:'full'
+  
+  currentBuild.displayName = pckg+currentBuild.displayName
 }
 return this
