ef75c03eee42b1c4bf11c3d75d9e9f56fd12daad
vishnunc <31437273+vishnunc@users.noreply.github.com>
2018-01-02 06:47:09 +0530

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,5 +1,25 @@
-def prep(){
-  stage('prep'){
+def pckg=""
+def setUp(){
+  stage('setUp'){
+  def me =sh (script:'git diff HEAD^ HEAD --name-only',returnStdout:true).trim().split('/')
+  pckg= me.length>1?me[1]:'full'
+  
+  currentBuild.displayName = pckg+currentBuild.displayName
+  }
+}
+def start(){
+  if(pckg=='full'){
+    print 'full'
+    def pipeline=load 'pipelines/lambda.groovy'
+    pipeline.start()
+  }
+  else
+  {
+    print pckg
+  }
+}
+def tearDown(){
+  stage('tearDown'){
   def me =sh (script:'git diff HEAD^ HEAD --name-only',returnStdout:true).trim().split('/')
   def pckg= me.length>1?me[1]:'full'
   
