b2ece1fd8a9fdeede27275ff24a356f782631ffa
vishnunc <31437273+vishnunc@users.noreply.github.com>
2018-01-02 07:17:34 +0530

    Update ui-web.groovy

--- ui-web.groovy
+++ ui-web.groovy
@@ -1,7 +1,16 @@
 def start(){
-  stage('uiweb'){
-    print 'i am here'
-  }
+  //Build the app
+  //Run the tests to check if build is good to go to dev
+  //Deploy to dev test and run tests again to make sure everything is fine after deployment
+  //Deploy to dev live and run tests
+  //Upload to artifactory or s3
+  //Deploy to int test and run tests to make sure everything is fine after deployment
+  //Deploy to int live and run tests
+  //Deploy to stg test and run tests to make sure everything is fine after deployment
+  //Deploy to stg live and run tests
+  //Deploy to prod blue and run tests to ensure everything is fine after deployment
+  //Do switch over after blue is fine
+  print 'this is ui-web pipeline'
 }
 
 return this
